b5b1bfe5dfd1de2c8a1876eb15aabad4bcdd1403
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-12 19:09:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log("The animal makes a sound.");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+const animal = new Animal("Tiger");
+console.log(animal.species);  // Output: Tiger
+animal.makeSound();  // Output: The animal makes a sound.
+
+const cat = new Cat("Persian");
+console.log(cat.species);  // Output: Persian
+cat.makeSound();  // Output: The animal makes a sound.
+cat.purr();  // Output: purr
+
+const dog = new Dog("Golden Retriever");
+console.log(dog.species);  // Output: Golden Retriever
+dog.makeSound();  // Output: The animal makes a sound.
+dog.bark();  // Output: woof
+
 
 // Do not change the code below this line
 window.Animal = Animal;
