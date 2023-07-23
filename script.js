e206ff34487e50df87bbd4b09af9d2141327c5c8
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-23 18:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 class Animal {
   constructor(species) {
-    this._species = species;
+    this.species = species;
   }
 
   get species() {
-    return this._species;
+    return this.species;
   }
 
   makeSound() {
-    console.log(`The ${_species} makes a sound`);
+    console.log(`The ${species} makes a sound`);
   }
 }
 
 class Cat extends Animal {
   purr() {
-    console.log(`purr`);
+    console.log("purr");
   }
 }
 
